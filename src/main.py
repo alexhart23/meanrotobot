6ca4cb84ded84f8d30bd6f5a9f860d2217d6ffc7
alexhart23 <alexhart23@gmail.com>
2015-09-08 22:23:51 -0400

    Updating requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,10 +1,36 @@
 __author__ = 'Alex Hart'
 
-from src import configs, parse_input, compose_reponse
+from src import configs, parse_input, compose_reponse, secrets
+import requests
 import sys
 
-def respond(tweet_text,user,tweet_id):
+# submit the results to a google form for tracking purposes AKA logging
+def submit_results(form_id, user, raw_tweet,
+                   tagged_tweet,
+                   question,
+                   confirmed_players,
+                   selections,
+                   category,
+                   response,
+                   tweet_id):
+    url = "https://docs.google.com/forms/d/%s/formResponse" % form_id
+    submission = {"entry.2105335826": user,
+                  "entry.1575116797": raw_tweet,
+                  "entry.845787665": tagged_tweet,
+                  "entry.1488885322": question,
+                  "entry.2116497517": confirmed_players,
+                  "entry.1779266700": selections,
+                  "entry.1211763545": category,
+                  "entry.468172303": response,
+                  "entry.1793373159": tweet_id,
+                  }
+
+    requests.post(url, submission)
+
+
+def respond(tweet_text, user, tweet_id):
     tweet = tweet_text.lower()
+    tokens = parse_input.tokenize_input(tweet_text)
     question = parse_input.identify_question(tweet)
     # TODO use different rankings based on ppr and return yards
     ppr = parse_input.is_ppr(tweet)
@@ -22,20 +48,31 @@ def respond(tweet_text,user,tweet_id):
         print("Is it return yards? %s" % return_yards)
 
         confirmed_players = parse_input.verify_possible_players(tweet_text,
-                                                            configs.nicknames,
-                                                            configs.rankings)
+                                                                configs.nicknames,
+                                                                configs.rankings)
         print("\nPlayers confirmed: %s" % confirmed_players)
-        players_with_info = parse_input.populate_player_info(confirmed_players,configs.rankings)
-        selections,player_options = parse_input.return_selection(players_with_info, picks)
+        players_with_info = parse_input.populate_player_info(confirmed_players,
+                                                             configs.rankings)
+        selections, player_options = parse_input.return_selection(
+            players_with_info, picks)
         print("\nSelection(s) are: %s" % selections)
         category = compose_reponse.determine_response_category(player_options)
         print("Category is: %s" % category)
-        response, response_len = compose_reponse.compose_tweet(selections,category,user)
+        response, response_len = compose_reponse.compose_tweet(selections,
+                                                               category, user)
         print("Response is: %s" % response)
-        # TODO submit raw tweet, user, identified question, confirmed players,selected players, and response to a google form for tracking
+
+        submit_results(form_id=secrets.FORM_ID,
+                       user=user,
+                       raw_tweet=tweet_text,
+                       tagged_tweet=tokens,
+                       question=question,
+                       confirmed_players=confirmed_players,
+                       selections=selections,
+                       category=category,
+                       response=response,
+                       tweet_id=tweet_id)
         return response
     elif question == "unknown":
         print("question unknown")
         sys.exit(1)
-
-
---
+++ src/secrets.py
@@ -0,0 +1,10 @@
+__author__ = 'Alex Hart'
+
+# twitter keys
+APP_KEY = 'fillmeout' #consumer key
+APP_SECRET = 'fillmeout' #consumer secret
+OAUTH_TOKEN = 'fillmeout'
+OAUTH_TOKEN_SECRET = 'fillmeout'
+
+# google form
+FORM_ID = 'fillmeout'
