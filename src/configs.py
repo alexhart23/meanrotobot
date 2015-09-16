54368831b682e87b50e3575ab6f77644c43ad0a6
alexhart23 <alexhart23@gmail.com>
2015-09-16 17:20:10 -0400

    Changing large_gap questions to give a selection regardless of how stupid the question is

--- src/configs.py
+++ src/configs.py
@@ -1,6 +1,7 @@
 __author__ = 'Alex Hart'
 
-week = "1"
+week = "2"
 rankings = ("rankings/week%s_projections.csv") % week
+ppr_rankings = ("rankings/week%s_projections_ppr.csv") % week
 test_rankings = "rankings/weekTEST_projections.csv"
 nicknames = "rankings/player_nicknames.csv"
