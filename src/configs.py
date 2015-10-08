2409f591f68dbc43e31383245f253c30f033a1c6
alexhart23 <alexhart23@gmail.com>
2015-10-08 10:17:20 -0400

    Adding week 5 projections

--- src/configs.py
+++ src/configs.py
@@ -1,6 +1,6 @@
 __author__ = 'Alex Hart'
 
-week = "4"
+week = "5"
 rankings = ("rankings/week%s_projections.csv") % week
 ppr_rankings = ("rankings/week%s_projections_ppr.csv") % week
 test_rankings = "rankings/weekTEST_projections.csv"
