07215f924ad0b92a98f5e594464d7fd92ab5871f
alexhart23 <alexhart23@gmail.com>
2015-11-16 18:16:07 -0500

    Adding week 8 rankings

--- src/configs.py
+++ src/configs.py
@@ -1,6 +1,6 @@
 __author__ = 'Alex Hart'
 
-week = "8"
+week = "10"
 rankings = ("rankings/week%s_projections.csv") % week
 ppr_rankings = ("rankings/week%s_projections_ppr.csv") % week
 test_rankings = "../rankings/weekTEST_projections.csv"
